1dba9adc79bd6fdbe1ef35f85bfe78a8e8d2c17d
myname <myname@code.com>
2025-08-09 12:00:04 -0700

    added / and urls

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/home")
 def hello():
     return render_template("file3.html")
     #return "Hello, World!"
 @app.route("/form_submission")
 def form_sub():
     return render_template("File1.html")
-
+@app.route("/")
+def home():
+    return render_template("home.html")
 if __name__ == "__main__":
     app.run(debug=True)
