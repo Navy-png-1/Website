9ac4efcc2d10d7d20581880fd74f1963ceb7d401
myname <myname@code.com>
2025-08-09 11:39:45 -0700

    new file:   app.py 	new file:   static/file2.css 	new file:   templates/File1.html 	new file:   templates/file3.html

---
+++ app.py
@@ -0,0 +1,16 @@
+# save this as app.py
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("file3.html")
+    #return "Hello, World!"
+@app.route("/form_submission")
+def form_sub():
+    return render_template("File1.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
