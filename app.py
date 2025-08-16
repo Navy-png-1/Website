dcf487f63c6d6d29ed0ff420fd9ff32f6ec3d02b
myname <myname@code.com>
2025-08-16 12:00:59 -0700

    journal

--- app.py
+++ app.py
@@ -3,11 +3,16 @@ from flask import Flask, render_template
 
 
 app = Flask(__name__)
-
+posts = [{"id":"11", "date":"8/16/2025", "content":"Info", "name":"Personal Info", "title":"Don't Know"}]
 @app.route("/home")
 def hello():
     return render_template("file3.html")
-    #return "Hello, World!"
+@app.route("/journal")
+def journal():
+    return render_template("file4.html")
+@app.route("/journal/posts")
+def posts():
+    return render_template("posts.html", posts=posts)
 @app.route("/form_submission")
 def form_sub():
     return render_template("File1.html")
