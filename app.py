9b3d793ad6465bb5f18635a20d6d16af8f4c3b3b
Navy-png-1 <navythewriter09@gmail.com>
2025-09-13 19:02:21 +0000

    time, cards, new_posts

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request
-from datetime import date
+from flask import Flask, render_template, request, url_for, send_from_directory
+from datetime import date, time
+import os
 
 app = Flask(__name__)
-posts = [{"id":"11", "date":"8/16/2025", "content":"Info", "name":"Personal Info", "title":"Don't Know"}, {"id":"11", "date":"8/16/2025", "content":"Info", "name":"Personal Info", "title":"Don't Know"}]
+posts = [{"id":"11", "date":"8/16/2025", "content":"Info", "name":"Personal Info", "title":"Don't Know"}, {"id":"12", "date":"9/13/2025", "content":"Information", "name":"Name", "title":"Title"}]
+
 @app.route("/test")
 def hello():
     return render_template("test.html")
@@ -15,12 +17,12 @@ def view_post():
 @app.route("/journal/new", methods=['GET', 'POST'])
 def new_post():
     #http://127.0.0.1:5000/journal/new?title=Title&name=Name&content=Content
-    if request.method=='POST':
+    if request.method=='GET':
         title = request.form.get('title')
         name = request.form.get('name')
         content = request.form.get('content')
         id = len(posts) + 1
-        date = datetime.now.strftime("mm/dd/yyyy HH:MM:SS")
+        date = time.strftime("%A %d. %B %Y")
     return render_template("new_posts.html")
 @app.route("/form_submission")
 def form_sub():
