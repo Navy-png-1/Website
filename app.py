e53cdd07b1a9a6f20f30b492de4f1997f82a7aa9
Navy-png-1 <navythewriter09@gmail.com>
2025-09-20 19:00:40 +0000

    file

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, request, url_for, send_from_directory
-from datetime import date, time
+from datetime import date, time, datetime
 import os
 
 app = Flask(__name__)
-posts = [{"id":"11", "date":"8/16/2025", "content":"Info", "name":"Personal Info", "title":"Don't Know"}, {"id":"12", "date":"9/13/2025", "content":"Information", "name":"Name", "title":"Title"}]
-
+posts = [{}]
 @app.route("/test")
 def hello():
     return render_template("test.html")
@@ -17,12 +16,16 @@ def view_post():
 @app.route("/journal/new", methods=['GET', 'POST'])
 def new_post():
     #http://127.0.0.1:5000/journal/new?title=Title&name=Name&content=Content
-    if request.method=='GET':
+    if request.method=='POST':
         title = request.form.get('title')
         name = request.form.get('name')
         content = request.form.get('content')
         id = len(posts) + 1
-        date = time.strftime("%A %d. %B %Y")
+        date = datetime.now().strftime("%A %d. %B %Y")
+        file = request.files.get('file')
+        filename = file.filename
+        file.save("uploads/"+filename)
+        posts.append({"id": id, "date":date,"content":content,"name": name, "title":title, "file":file})
     return render_template("new_posts.html")
 @app.route("/form_submission")
 def form_sub():
